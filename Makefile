d800365193a5f6d9f7da884b0d6522a3bda4287a
Truong Bui <tbui0936@gmail.com>
2023-12-19 23:56:43 +0700

    feat: handle error, add user sqlc

--- Makefile
+++ Makefile
@@ -9,11 +9,22 @@ dropdb:
 migrateup:
 	migrate -path db/migration -database "postgresql://root:secret@localhost:5432/bank_transf?sslmode=disable" -verbose up
 
+migrateup1:
+	migrate -path db/migration -database "postgresql://root:secret@localhost:5432/bank_transf?sslmode=disable" -verbose up 1
+
+
 migratedown:
 	migrate -path db/migration -database "postgresql://root:secret@localhost:5432/bank_transf?sslmode=disable" -verbose down
 
+migratedown1:
+	migrate -path db/migration -database "postgresql://root:secret@localhost:5432/bank_transf?sslmode=disable" -verbose down 1
+
+
 sqlc:
 	sqlc generate
+
+sqlcu:
+	docker run --rm -v ${pwd}:/src -w /src kjconroy/sqlc generate
 
 test:
 	go test -v -cover ./...
@@ -24,4 +35,4 @@ server:
 mock:
 	mockgen -package mockdb -destination db/mock/store.go github.com/BuiNhatTruong99/Go-Simple-Bank-/db/sqlc Store
 
-.PHONY: posgres createdb dropdb migrateup migratedown sqlc test server mock
+.PHONY: posgres createdb dropdb migrateup migratedown migrateup1 migratedown1 sqlc test server mock
---
+++ db/sqlc/user_test.go
@@ -0,0 +1,52 @@
+package db
+
+import (
+	"context"
+	"github.com/BuiNhatTruong99/Go-Simple-Bank-/util"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func createRandomUser(t *testing.T) User {
+	arg := CreateUserParams{
+		Username:     util.RandomOwner(),
+		HashPassword: "secret",
+		FullName:     util.RandomOwner(),
+		Email:        util.RandomEmail(),
+	}
+
+	user, err := testQueries.CreateUser(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, user)
+
+	require.Equal(t, arg.Username, user.Username)
+	require.Equal(t, arg.HashPassword, user.HashPassword)
+	require.Equal(t, arg.FullName, user.FullName)
+	require.Equal(t, arg.Email, user.Email)
+
+	require.NotZero(t, user.CreatedAt)
+	require.True(t, user.PasswordChangeAt.IsZero())
+
+	return user
+}
+
+func TestCreateUser(t *testing.T) {
+	createRandomUser(t)
+}
+
+func TestGetUser(t *testing.T) {
+	user := createRandomUser(t)
+
+	getuser, err := testQueries.GetUser(context.Background(), user.Username)
+	require.NoError(t, err)
+	require.NotEmpty(t, getuser)
+
+	require.Equal(t, getuser.Username, user.Username)
+	require.Equal(t, getuser.HashPassword, user.HashPassword)
+	require.Equal(t, getuser.FullName, user.FullName)
+	require.Equal(t, getuser.Email, user.Email)
+	require.WithinDuration(t, user.CreatedAt, getuser.CreatedAt, time.Millisecond)
+	require.WithinDuration(t, user.PasswordChangeAt, getuser.PasswordChangeAt, time.Millisecond)
+
+}
